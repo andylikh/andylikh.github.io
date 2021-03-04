b204a2e0e357ebc452a88f567ac0e17405c860ad
And Li <39035665+zxcvb7653@users.noreply.github.com>
2021-03-04 15:20:28 +0000

    Update Skill.js

--- src/components/skill/Skill.js
+++ src/components/skill/Skill.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const imageMap = {
-    figma: 'images/skills/icon_figma.png',
+    figma: '/images/skills/icon_figma.png',
     system:
         'https://cdn2.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-7/24/Untitled-8-512.png'
 };
