1ae0d8eeb9dfdf8a64be069a46c4301200d3127e
And Li <39035665+zxcvb7653@users.noreply.github.com>
2021-03-04 14:58:54 +0000

    Updates

--- src/components/websiteHeader/WebsiteHeader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Navbar from '../navbar/Navbar';
-import styles from './WebsiteHeader.module.css';
-
-export default function WebsiteHeader() {
-    return <Navbar className={styles.navBar} />;
-}
